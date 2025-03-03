f8fb1bc7302f43f253f944d250374ed8e7a3148d
Arsh Patel <arshpatel213@gmail.com>
2025-03-03 09:55:03 +0530

    Time: 6 ms, Memory: 42.9 MB - LeetHub

---
+++ 6807-317-257-binary-tree-paths/6807-317-257-binary-tree-paths.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<String>();
+        if(root == null){
+            return res;
+        }
+        StringBuilder sb = new StringBuilder();
+        dfs(root, res, sb);
+        return res;
+    }
+    private void dfs(TreeNode root, List<String> res, StringBuilder sb){
+        if(root.left == null && root.right == null){
+            sb.append(""+ root.val);
+            res.add(sb.toString());
+            return;
+        }
+        if(root.left != null){
+            String prev = sb.toString();
+            sb.append(root.val + "->");
+            dfs(root.left, res, sb);
+            sb = new StringBuilder(prev);
+        }
+        if(root.right != null){
+            sb.append(root.val + "->");
+            dfs(root.right, res, sb);
+        }
+    }
+}
